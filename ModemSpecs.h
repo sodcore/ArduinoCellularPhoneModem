c5493ecaa6f6e3c584f1f2d2c23efe5c76065af2
sodcore <38105732+sodcore@users.noreply.github.com>
2024-04-23 14:34:45 -0600

    Update ModemSpecs.h

--- ModemSpecs.h
+++ ModemSpecs.h
@@ -8,11 +8,11 @@
 #ifndef MODEMSPECS_H_
 #define MODEMSPECS_H_
 
-#define WEB_SITE "www.binmass.com" // The web Site the the modem is sending http post to
+#define WEB_SITE "www.expample.com" // The web Site the the modem is sending http post to
 #define WEB_PORT 80 // The port that the modem uses to access the web-server
-//#define CGI_BIN "/cgi-bin/farm/coordinator/" // The cgi-bin we use on the web-site for uploading data
-#define CGI_BIN "/cgi-bin/farm/testCoordinator/coordinator/" // The cgi-bin we use on the web-site for uploading data
-#define HTTP_TOKEN_LOCK "IiPdT8ktkkECndG8C1sa885mZ6OwqFQKtrsDaHFUj4"
+//#define CGI_BIN "/cgi-bin/farm" // The cgi-bin we use on the web-site for uploading data
+#define CGI_BIN "/cgi-bin/farm" // The cgi-bin we use on the web-site for uploading data
+#define HTTP_TOKEN_LOCK "64bitrandom string"
 // The time between getting information from the device communication in cycles of
 #define MAX_TIME_BETWEEN_COMMUNICATION 3600000//900000////120000//3600000
 
